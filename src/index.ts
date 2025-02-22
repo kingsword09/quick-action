576babc7c293fd2cbf1e2d9ddbaadfe8829eccf1
kingsword09 <kingsword09@gmail.com>
2025-02-22 10:16:02 +0800

    🐛 fix Command Palette error

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import { spawn } from 'node:child_process'
+import path from 'node:path'
 import { glob } from 'glob'
 import { defineExtension, extensionContext } from 'reactive-vscode'
-import { Uri, commands, workspace } from 'vscode'
+import { Uri, commands, window, workspace } from 'vscode'
 import { recordToAsyncIterable } from './utils'
 
 const { activate, deactivate } = defineExtension(() => {
   const disposable = commands.registerCommand('quick.open.in.default.program', async (dir) => {
-    const selectDirPath = dir instanceof Uri ? dir.fsPath : ''
+    let selectDirPath = dir instanceof Uri ? dir.fsPath : ''
+
+    if (selectDirPath === '') {
+      const currentFilePath = window.activeTextEditor?.document.uri.fsPath ?? ''
+      if (currentFilePath === '') {
+        window.showErrorMessage('No selection directory or active file.')
+        return
+      }
+
+      selectDirPath = path.dirname(currentFilePath)
+    }
+
     let programName = 'Visual Studio Code'
 
     const programActions: Record<string, string> = workspace.getConfiguration('quick-action').get('programActions') ?? {}
